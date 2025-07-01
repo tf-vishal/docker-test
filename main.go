e9c907771c9b09899b633004d279a7a13a19655e
tf-vishal <vvishalsharma0609@gmail.com>
2025-07-01 11:19:50 +0530

    Added the server config...

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"html"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hi")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
